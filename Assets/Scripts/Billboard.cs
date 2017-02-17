8600c6fc5e8df004fafa4c13f8fd7468f048376c
Robert <rob5300@hotmail.com>
2017-02-17 20:25:49 +0000

    Added new Scripts (NPC, StoryNPC, Billboard). Added base NPC prefab to begin tests.

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour{
+	
+	void LateUpdate()
+	{
+		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.back,
+		                 						Camera.main.transform.rotation * Vector3.up);
+	}
+}
