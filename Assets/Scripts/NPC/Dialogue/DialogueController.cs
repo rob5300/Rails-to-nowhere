e706a5670d5c84e8c4ba99f211dc294858ae58a0
Robert <rob5300@hotmail.com>
2017-02-17 23:23:56 +0000

    Created DialogueNode and DialogueController. Require more work but its a start.

---
+++ Assets/Scripts/NPC/Dialogue/DialogueController.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public class DialogueController{
+
+    public Dictionary<string, DialogueNode> DialogueNodeLibrary = new Dictionary<string, DialogueNode>();
+
+}
---
+++ Assets/Scripts/NPC/Dialogue/DialogueNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class DialogueNode {
+    public string Text = "";
+    public string Name = "New Node";
+    public string Optiontitle;
+    //key is the shown text on the response button, value is the name of the corrisponding dialoguenode.
+    public Dictionary<string, string> Responses = new Dictionary<string, string>();
+
+    public DialogueNode(string name, string text) {
+        Name = name;
+        Text = text;
+    }
+}
