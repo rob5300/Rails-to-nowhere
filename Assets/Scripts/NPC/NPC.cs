fe43f761d7848c3d991f7dd14eebca9fc9f83539
Matt Jones <matthewj866@gmail.com>
2017-02-18 01:47:52 +0000

    Merging attempt fix.

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public abstract class NPC : MonoBehaviour {
+
+    public string Name = "New NPC";
+    public bool Interactable = true;
+    public float Health = 1;
+
+    public abstract void Interact();
+
+    public void OnTaskComplete() {
+
+    }
+
+    public void OnTaskBegin() {
+
+    }
+
+    public void OnTaskFail() {
+
+    }
+
+    public virtual void Damage(float damage) {
+        if (Health - damage <= 0) {
+            Die();
+        }
+        else {
+            Health -= damage;
+        }
+    }
+
+    public void Die() {
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/NPC/StoryNPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class StoryNPC : NPC {
+
+	public bool Essential = false;
+
+    public override void Damage(float damage) {
+        if (Health - damage <= 0) {
+            Health = 0;
+        }
+        else {
+            Health -= damage;
+        }
+    }
+
+    public override void Interact() {
+        Debug.Log("Interacted with: " + Name);
+    }
+}
