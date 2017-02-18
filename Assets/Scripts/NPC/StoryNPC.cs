c8ad23aa8162f8a57294df3b387f1d38201061a3
Robert <rob5300@hotmail.com>
2017-02-18 17:25:36 +0000

    Completed DialogueController and DialogueNode. Should be ready for use.

--- Assets/Scripts/NPC/StoryNPC.cs
+++ Assets/Scripts/NPC/StoryNPC.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 public class StoryNPC : NPC {
 
 	public bool Essential = false;
+    public string InitialDialogueNodeName;
+    public bool HasSpeach { get {
+            if(InitialDialogueNodeName == null) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }}
 
     public override void Damage(float damage) {
         if (Health - damage <= 0) {
             Health = 0;
+            if (!Essential) {
+                Die();
+            }
         }
         else {
             Health -= damage;
