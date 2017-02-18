6c20a3028a861536130e81e0d68ba7bdf934807a
DESKTOP-EG3IGD1\Maria <ayurici@gmail.com>
2017-02-18 18:07:08 +0000

    Added player controls and Object Interaction functionality (partially).

---
+++ Assets/Scripts/Mari's player and objects/ObjHover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjHover : MonoBehaviour {
+    public string collidedmesh;
+
+    bool isHovering;
+    
+    void Start()
+    {
+        collidedmesh = transform.name;
+    }
+    void OnMouseEnter()
+    {
+        isHovering = true;
+    }
+
+    void OnMouseExit()
+    {
+        isHovering = false;
+    }
+
+    void OnGUI()
+    {
+        if (isHovering == true)
+        {
+            GUI.Box(new Rect(300, 100, 100, 20), "Pick me up!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
