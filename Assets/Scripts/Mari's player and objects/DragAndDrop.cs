af2f2f823947ad866c9162a420ad11f9045d9c8e
Matt Jones <matthewj866@gmail.com>
2017-02-19 10:11:35 +0000

    Stage 1 of bug fixing Maria's drag and drop code - pickup now works, drop does not.

--- Assets/Scripts/Mari's player and objects/DragAndDrop.cs
+++ Assets/Scripts/Mari's player and objects/DragAndDrop.cs
@@ -10,6 +10,7 @@ public class DragAndDrop : MonoBehaviour
     bool hasPlayer = false;
     bool beingCarried = false;
     private bool touched = false;
+	private float _timeSinceCarry = 0f;
 
     void Start()
     {
@@ -18,8 +19,9 @@ public class DragAndDrop : MonoBehaviour
 
     void Update()
     {
+		_timeSinceCarry += Time.deltaTime;
         float dist = Vector3.Distance(gameObject.transform.position, player.position); //checks distance between game object and player
-        if (dist <= 2.5f)
+        if (dist <= 5f)
         {
             hasPlayer = true; //if distance is less or equal to 2.5, object can be picked up
         }
@@ -27,36 +29,42 @@ public class DragAndDrop : MonoBehaviour
         {
             hasPlayer = false;
         }
-        if (hasPlayer && Input.GetKeyDown(KeyCode.W)) //if the object can be picked up, it will follow the camera
+        if (hasPlayer && Input.GetMouseButtonDown(1)) //if the object can be picked up, it will follow the camera
         {
             GetComponent<Rigidbody>().isKinematic = true;
             transform.parent = playerCam;
             beingCarried = true;
         }
-        if (beingCarried)
-        {
-            if (touched)
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                touched = false;
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+		else if (beingCarried && _timeSinceCarry > 1f && Input.GetMouseButtonDown(1))
+		{
+			GetComponent<Rigidbody>().isKinematic = false;
+			transform.parent = null;
+			beingCarried = false;
+		}
+        //if (beingCarried) -- what is this doing?
+        //{
+        //    if (touched)
+        //    {
+        //        GetComponent<Rigidbody>().isKinematic = false;
+        //        transform.parent = null;
+        //        beingCarried = false;
+        //        touched = false;
+        //    }
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        GetComponent<Rigidbody>().isKinematic = false;
+        //        transform.parent = null;
+        //        beingCarried = false;
+        //        GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
        
-            }
-            else if (Input.GetMouseButtonDown(1))
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-            }
-        }
+        //    }
+        //    else if (Input.GetMouseButtonDown(1))
+        //    {
+        //        GetComponent<Rigidbody>().isKinematic = false;
+        //        transform.parent = null;
+        //        beingCarried = false;
+        //    }
+        //}
     }
 
     void OnTriggerEnter() //if the object touches something else, it falls down
