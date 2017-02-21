66b3be60438a56a6c6c9cb5a5ec3805ae995ba45
Robert <rob5300@hotmail.com>
2017-02-21 23:07:18 +0000

    Added Ubuntu Font, New Player class, new UI class. Began creation of speech UI.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public static Player player;
+
+    void Start() {
+        //Assign the static variable player to the only instance of this class that should exist.
+        if (!player) player = this;
+        else Debug.LogError("Multiple player instances on objects: " + player.name + ", " + name);
+    }
+}
