9154d51bdb51d20dbccd2bc38a90bd45e1471dec
Matt Jones <matthewj866@gmail.com>
2017-02-27 18:00:36 +0000

    Refactored and documented relevant code for Rob's use.

--- Assets/Scripts/Tools and Components/BaseTool.cs
+++ Assets/Scripts/Tools and Components/BaseTool.cs
@@ -13,6 +13,9 @@ public abstract class BaseTool : MonoBehaviour {
 	protected bool _move = false;
 	protected Vector3 _mouseInWorldSpace; //this is in case a method is overidden that needs this value
 
+	/// <summary>
+	/// The prefab to be spawned by the BaseTool instance.
+	/// </summary>
 	public GameObject Prefab
 	{
 		get
@@ -26,6 +29,10 @@ public abstract class BaseTool : MonoBehaviour {
 		}
 	}
 
+
+	/// <summary>
+	/// The target camera to use.
+	/// </summary>
 	public Camera PuzzleCam
 	{
 		get
@@ -39,7 +46,9 @@ public abstract class BaseTool : MonoBehaviour {
 		}
 	}
 
-	// Use this for initialization
+	/// <summary>
+	/// Base implementation checks the puzzle instance in the world and loads it into the BaseTool's private fields. You may want to consider overriding this if there is multiple puzzles in the world.
+	/// </summary>
 	protected virtual void Start()
 	{
 		_blocks = GameObject.FindGameObjectWithTag("Board").transform.GetComponentsInChildren<EngComponent>().ToList();
@@ -51,11 +60,43 @@ public abstract class BaseTool : MonoBehaviour {
 		}
 	}
 
-	// Update is called once per frame
+	/// <summary>
+	/// Base implementation removes empty blocks within a puzzle, gets the mouse in world and assigns it to _mouseInWorldSpace, then checks if the player has picked up the tool by calling CheckMove(), then based on CheckMove() changing the value of _move to true or false, it will then follow the mouse cursor and check the Place/Remove calls.
+	/// </summary>
 	protected virtual void Update()
 	{
 		_blocks.RemoveAll(x => x == null);
 		_mouseInWorldSpace = PuzzleCam.ScreenToWorldPoint(Input.mousePosition);
+		CheckMove();
+		if (_move)
+		{
+			_mouseInWorldSpace.z = transform.position.z;
+			transform.position = _mouseInWorldSpace;
+			CheckPlaceRemoveCalls();
+		}
+	}
+
+	/// <summary>
+	/// Base implementation checks if the player is trying to place or remove a component by checking MB1 and MB2 respectively.
+	/// </summary>
+	protected virtual void CheckPlaceRemoveCalls()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			PlaceObject(_mouseInWorldSpace);
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			RemoveObject(_mouseInWorldSpace);
+		}
+	}
+
+
+	/// <summary>
+	/// Base implementation uses a GetKeyDown call on the E key to run the code required to pick up a tool within 2D space. You may want to override this if it's 3D.
+	/// </summary>
+	protected virtual void CheckMove()
+	{
 		if (Input.GetKeyDown(KeyCode.E) && !_move && Vector2.Distance(_mouseInWorldSpace, transform.position) < (GetComponent<Renderer>().bounds.size.x / 2) + 0.02f)
 		{
 			_move = true;
@@ -64,24 +105,13 @@ public abstract class BaseTool : MonoBehaviour {
 		{
 			_move = false;
 		}
-		if (_move)
-		{
-
-			_mouseInWorldSpace.z = transform.position.z;
-			transform.position = _mouseInWorldSpace;
-			if (Input.GetMouseButtonDown(0))
-			{
-				PlaceObject(_mouseInWorldSpace);
-			}
-			if (Input.GetMouseButtonDown(1))
-			{
-				RemoveObject(_mouseInWorldSpace);
-			}
-		}
-
 	}
 
-	protected virtual void RemoveObject(Vector3 position = default(Vector3))
+	/// <summary>
+	/// Base implementation uses positions to remove 1 object that has an EngComponent object instances and is of the prefab type.
+	/// </summary>
+	/// <param name="position">Position is traditionally the mouse in world position. See base.Update().</param>
+	protected virtual void RemoveObject(Vector3 position)
 	{
 		GameObject closestObj = _blocks.OrderBy(x => Vector2.Distance(position, x.transform.position)).First().gameObject;
 		if (Vector2.Distance(closestObj.transform.position, position) < 1 && closestObj.tag == Prefab.tag && closestObj.name.Contains(Prefab.name))
@@ -92,7 +122,11 @@ public abstract class BaseTool : MonoBehaviour {
 		}
 	}
 
-	protected virtual void PlaceObject(Vector3 position = default(Vector3))
+	/// <summary>
+	/// Base implementation uses positions to add 1 object of the prefab type.
+	/// </summary>
+	/// <param name="position">Position is traditionally the mouse in world position. See base.Update().</param>
+	protected virtual void PlaceObject(Vector3 position)
 	{
 		GameObject closestBoardObj = _boardAreas.OrderBy(x => Vector2.Distance(position, x.transform.position)).First();
 		GameObject closestCircuitObj = _blocks.OrderBy(x => Vector2.Distance(position, x.transform.position)).First().gameObject;
--- Assets/Scripts/Tools and Components/EngComponent.cs
+++ Assets/Scripts/Tools and Components/EngComponent.cs
@@ -9,6 +9,10 @@ public abstract class EngComponent : MonoBehaviour
 	[SerializeField]
 	protected bool _connected = false;
 
+
+	/// <summary>
+	/// Checks if the component is connected to a valid path to the starting point.
+	/// </summary>
 	public bool Connected
 	{
 		get
@@ -22,6 +26,9 @@ public abstract class EngComponent : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// calls CheckConnections every frame. Ensure you call base.Update whenever you are inheriting from EngComponent so that the check is called.
+	/// </summary>
 	protected virtual void Update()
 	{
 		_updateResults = CheckConnections();
@@ -31,7 +38,11 @@ public abstract class EngComponent : MonoBehaviour
 		}
 	}
 
-	protected List<RaycastHit2D> CheckConnections()
+	/// <summary>
+	/// Base implementation uses a Physics2D.CircleCastAll to an appropriate size based on the X-size of the Renderer. Every other class relies on this, do not override unless completely essential.
+	/// </summary>
+	/// <returns></returns>
+	protected virtual List<RaycastHit2D> CheckConnections()
 	{
 		return Physics2D.CircleCastAll(transform.position, (GetComponent<Renderer>().bounds.size.x / 2) + 0.01f, Vector2.zero, 0f).Where(x => x.transform.tag == "Circuit").ToList();
 	}
