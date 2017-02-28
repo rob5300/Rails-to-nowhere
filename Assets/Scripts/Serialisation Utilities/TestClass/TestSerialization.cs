44e23fe3a3e27c6e15b8f772bd398544370742cf
Matt Jones <matthewj866@gmail.com>
2017-02-28 19:11:17 +0000

    Added functionality to serialise any MonoBehaviour into XML file. Added new test scene and class for XML file. Altered NPC to be serialisable by new serialiser functionality.

---
+++ Assets/Scripts/Serialisation Utilities/TestClass/TestSerialization.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSerialization : MonoBehaviour {
+
+	private UnityXMLSerialiser<StoryNPC> _storyNPCSerialiser;
+	public GameObject NPC;
+
+	// Use this for initialization
+	void Start () {
+		_storyNPCSerialiser = new UnityXMLSerialiser<StoryNPC>();
+	}
+
+	public void Serialise()
+	{
+		print("fired");
+		_storyNPCSerialiser.SerialiseInstance(NPC.GetComponent<StoryNPC>(), new System.IO.FileInfo("test.xml"));
+
+	}
+
+}
