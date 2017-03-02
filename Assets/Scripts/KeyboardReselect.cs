0ffe4f6a010b39b6c2e07689038abc0e50ada3c1
Robert <rob5300@hotmail.com>
2017-03-02 21:26:48 +0000

    Added Carriage models and starting of scene. Added testing scene for main menu. KeyboardReselect.cs is the reselect script. Scene is Main menu testingifthisworks.

---
+++ Assets/Scripts/KeyboardReselect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeyboardReselect : MonoBehaviour {
+
+    public EventSystem eventsys;
+    public GameObject toSelect;
+	
+	void Update () {
+	    if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) {
+            if(eventsys.currentSelectedGameObject == null) {
+                eventsys.SetSelectedGameObject(toSelect);
+            }
+        }
+	}
+}
