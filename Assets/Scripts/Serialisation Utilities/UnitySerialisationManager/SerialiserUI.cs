931eb54200b604573d82852fd47a3fbed2a228e7
Matt Jones <matthewj866@gmail.com>
2017-03-02 23:48:53 +0000

    Fixed issue where serialiser UI would cause unity to die horribly. Fixed issue with changing Serialisable MonoBehaviour values.

--- Assets/Scripts/Serialisation Utilities/UnitySerialisationManager/SerialiserUI.cs
+++ Assets/Scripts/Serialisation Utilities/UnitySerialisationManager/SerialiserUI.cs
@@ -5,14 +5,18 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 public class SerialiserUI : EditorWindow
 {
+	private static int _oldCount = 0;
 	string myString = "New NPC";
 	bool groupEnabled;
 	bool myBool = true;
 	float myFloat = 1.23f;
-	private static Dictionary<IUnityXMLSerialisable, List<UnityEngine.Object>> _serialiseableObjectFields;
+	private static Dictionary<IUnityXMLSerialisable, List<UnityEngine.Object>> _serialiseableUnityObjectFields;
+	private static Dictionary<IUnityXMLSerialisable, List<object>> _serialisableObjectFields;
+	private static List<IUnityXMLSerialisable> _instances;
 	private static SerialiserUI _window;
 
 	// Add menu named "My Window" to the Window menu
@@ -27,10 +31,30 @@ public class SerialiserUI : EditorWindow
 
 	void OnGUI()
 	{
-		_serialiseableObjectFields = new Dictionary<IUnityXMLSerialisable, List<UnityEngine.Object>>();
-		DynamicallyLoadSerialisedFiles();
+		int newCount = Directory.GetFiles(Application.streamingAssetsPath).Where(x => x.Contains(".xml")).Count();
+
+		if (_instances == null || newCount != _oldCount)
+		{
+			IUnityXMLSerialisable[] resultColl = GameObject.Find("InstanceHolderObj").GetComponents<IUnityXMLSerialisable>();
+			foreach (var behaviour in resultColl)
+			{
+				DestroyImmediate((MonoBehaviour)behaviour);
+			}
+			DynamicallyLoadSerialisedFiles();
+			_oldCount = newCount;
+		}
+		else
+		{
+			foreach (IUnityXMLSerialisable instance in _instances)
+			{
+				DrawFields(instance);
+			}
+		}
+
+
 
 		//myString = EditorGUILayout.TextField("Text Field", myString);
+		//ditorGUILayout.Toggle()
 		//EditorGUILayout.
 		//groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
 		//myBool = EditorGUILayout.Toggle("Toggle", myBool);
@@ -40,6 +64,7 @@ public class SerialiserUI : EditorWindow
 
 	private static void DynamicallyLoadSerialisedFiles()
 	{
+		_instances = new List<IUnityXMLSerialisable>();
 		Type baseInterfaceType = typeof(IUnityXMLSerialisable);
 		Type baseSerialiserType = typeof(UnityXMLSerialiser<>);
 		List<Type> types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).Where(y => baseInterfaceType.IsAssignableFrom(y) && y != baseInterfaceType && !y.IsAbstract).ToList();
@@ -55,23 +80,54 @@ public class SerialiserUI : EditorWindow
 				string actualPath = file.Replace('/', '\\');
 				FileInfo fileInfo = new FileInfo(actualPath);
 				IUnityXMLSerialisable result = (IUnityXMLSerialisable)instantiatedSerialiser.GetType().GetMethod("DeserialiseXML").Invoke(instantiatedSerialiser, new object[2] { fileInfo, false });
-				List<string> targetProps = result.GetSerialiseTargets();
-				if (!_serialiseableObjectFields.Keys.Contains(result))
-				{
-					_serialiseableObjectFields.Add(result, new List<UnityEngine.Object>());
-				}
-				foreach (string prop in targetProps)
-				{
-					List<UnityEngine.Object> existingFields = _serialiseableObjectFields[result];
-					if (!existingFields.Where(x => x.name == prop).Any())
-					{
-						//var bla = EditorGUILayout.ObjectField(result.GetType().GetProperty(prop).GetValue(result, null), objType: result.GetType().GetProperty(prop).GetType(), allowSceneObjects: true);
-						//var bla = EditorGUILayout.ObjectField(label: "bla", obj: result.GetSerialiseTargets(), objType: typeof(List<String>), allowSceneObjects: true);
-						//	existingFields.Add(bla);
-					}
-				}
+				DrawFields(result);
 			}
 
 		}
 	}
+
+	private static void DrawFields(IUnityXMLSerialisable target)
+	{
+		List<string> targetProps = target.GetSerialiseTargets();
+		if (!_instances.Contains(target))
+		{
+			_instances.Add(target);
+		}
+		foreach (string prop in targetProps)
+		{
+			//if (!existingUnityFields.Where(x => x.name == prop).Any())
+			//{
+			PropertyInfo propInfo = target.GetType().GetProperty(prop);
+			if (propInfo.PropertyType.IsAssignableFrom(typeof(UnityEngine.Object)))
+			{
+				propInfo.SetValue(target, EditorGUILayout.ObjectField(prop + ":", (UnityEngine.Object)propInfo.GetValue(target, null), target.GetType(), true), null);
+			}
+			else if (propInfo.PropertyType.IsPrimitive || propInfo.PropertyType == typeof(string))
+			{
+				var methodGroup = typeof(EditorGUILayout).GetMethods().Where(x => (x.Name.Contains("Field") || x.Name.Contains("Toggle")) && x.IsStatic && !x.Name.Contains("Delay") && (x.ReturnType.IsPrimitive || x.ReturnType == typeof(string)) && !x.Name.Contains("Property"));
+				if (methodGroup.Where(x => x.ReturnType == propInfo.PropertyType).Any())
+				{
+					var methodToInvoke = methodGroup
+										.Where(x => x.ReturnType == propInfo.PropertyType && x.GetParameters().Count() == 3 && x.GetParameters()
+												.Where(y => y.GetCustomAttributes(typeof(ParamArrayAttribute), false).Length > 0).Any() && x.GetParameters()
+												.Where(z => z.Name == "label").Any()).First();
+					propInfo.SetValue(target, methodToInvoke.Invoke(null, new object[] { prop + ":", propInfo.GetValue(target, null), new GUILayoutOption[0] }), null);
+				}
+				else
+				{
+					Debug.Log("Non-monobehaviour type could not be serialised. Reason: Not primitive.");
+				}
+			}
+			//this.Repaint();
+		}
+		EditorGUILayout.Space();
+	}
+
+	//void OnValidate()
+	//{
+	//	foreach (IUnityXMLSerialisable instance in _instances)
+	//	{
+	//		DrawFields(instance);
+	//	}
+	//}
 }
