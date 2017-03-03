50f6ad8ba87e6e2d1a0fe13f1a80a7128eb4d536
Matt Jones <matthewj866@gmail.com>
2017-03-03 00:32:40 +0000

    Fixed issue with changing scene from pause menu causing game to not function correctly.

--- Assets/Scripts/Mari's player and objects/Menu Scripts/Main Menu/ChangeScenes.cs
+++ Assets/Scripts/Mari's player and objects/Menu Scripts/Main Menu/ChangeScenes.cs
@@ -6,6 +6,7 @@ public class ChangeScenes : MonoBehaviour {
 
     public void ChangeMyScene(string myScene) //when void is added, it's not going to return anything, just do something
     {
+		Time.timeScale = 1f;
         SceneManager.LoadScene(myScene);
 
         //a function which is gonna expect a string , which is going to be the name of another scene, and it's going to load that scene
