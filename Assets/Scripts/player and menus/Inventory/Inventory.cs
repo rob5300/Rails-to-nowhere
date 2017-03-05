fe4d2d4c4b4d78832123436ed2c369b3c87565de
DESKTOP-EG3IGD1\Maria <ayurici@gmail.com>
2017-03-05 16:52:29 +0000

    started working on the inventory

---
+++ Assets/Scripts/player and menus/Inventory/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Inventory {
+
+    public List<ItemSlot> ItemSlots = new List<ItemSlot>();
+
+    public Inventory(int capacity)
+    {
+        ItemSlots = new List<ItemSlot>(capacity);
+
+        ItemSlots.AddRange(Enumerable.Repeat(new ItemSlot(), capacity));
+    }
+
+    void AddItem()
+    {
+
+    }
+
+    void RemoveItem()
+    {
+
+    }
+
+    private class ItemSlot
+    {
+        public string itemID;
+        public int itemQuantity;
+    }
+}
+
+
---
+++ Assets/Scripts/player and menus/Inventory/Item.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Item {
+
+    public string itemID;
+    public string itemName;
+    public string itemInfo;
+    public Sprite itemImg;
+    public GameObject itemPrefab;
+
+    public Item(string name, string id, string info, Sprite img, GameObject prefab)
+    {
+        itemName = name;
+        itemID = id;
+        itemInfo = info;
+        itemImg = img;
+        itemPrefab = prefab;
+
+        ItemList.Add(id, this);
+    }
+
+   
+    public static Dictionary<string, Item> ItemList = new Dictionary<string, Item>();
+
+    public static Item box = new Item("Test item", "1", "I am a box that does nothing", Resources.Load<Sprite>("ItemsToPickup/Cubeimage"), Resources.Load<GameObject>("ItemsToPickup/Cube"));
+
+}
