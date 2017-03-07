7f8b2583789cb3d71480c91deab424747db4c994
Robert <rob5300@hotmail.com>
2017-03-07 21:59:00 +0000

    Testing files for UnitySerialiser system.

---
+++ Assets/Scripts/Serialisation Utilities/TestClass/InterfaceTesting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceTesting : MonoBehaviour, IUnityXMLSerialisable {
+
+    private List<int> intList = new List<int>();
+    private bool testBool = false;
+    private Vector3 transformOb = new Vector3(1f, 2f, 3f);
+    private List<GameObject> gameobList = new List<GameObject>();
+
+
+    public List<int> IntList {
+        get {
+            return intList;
+        }
+
+        set {
+            intList = value;
+        }
+    }
+
+    public bool TestBool {
+        get {
+            return testBool;
+        }
+
+        set {
+            testBool = value;
+        }
+    }
+
+    public Vector3 TransformOb {
+        get {
+            return transformOb;
+        }
+
+        set {
+            transformOb = value;
+        }
+    }
+
+    public List<GameObject> GameobList {
+        get {
+            return gameobList;
+        }
+
+        set {
+            gameobList = value;
+        }
+    }
+
+    public List<Func<object, object>> GetMappings(string propName) {
+        return new List<Func<object, object>>() { delegate(object o) { return ((DialogueNode)o).Key; }, delegate (object o) { return ((DialogueNode)o).Text; } };
+    }
+
+    public List<string> GetSerialiseTargets() {
+        return new List<string>() {"IntList", "TestBool", "TransformOb"};
+    }
+}
---
+++ Assets/Scripts/Serialisation Utilities/TestClass/TestSerialization1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq.Expressions;
+using System;
+
+public class TestSerialization1 : MonoBehaviour {
+
+	private UnityXMLSerialiser<InterfaceTesting> _serialiser;
+	public GameObject testob;
+
+	// Use this for initialization
+	void Start () {
+		_serialiser = new UnityXMLSerialiser<InterfaceTesting>();
+	}
+
+	public void Serialise()
+	{
+		_serialiser.SerialiseInstance(testob.GetComponent<InterfaceTesting>(), new System.IO.FileInfo(Application.streamingAssetsPath + @"\InterfaceTesting.xml"));
+
+	}
+
+	public void Deserialise()
+	{
+        InterfaceTesting deser = _serialiser.DeserialiseXML(new System.IO.FileInfo(Application.streamingAssetsPath + @"\InterfaceTesting.xml"));
+	}
+
+}
