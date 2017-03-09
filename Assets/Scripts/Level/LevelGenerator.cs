a3a7aa830a9fc0c97ea16e95f4605099e9ed46bc
Robert-Thinkpad <rob5300@hotmail.com>
2017-03-09 10:14:18 +0000

    Fixed Carriage generation to properly generate for odd numbered ratios. Also handles properly to avoid creating more filler carriages than specified.

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -15,6 +15,9 @@ public class LevelGenerator : MonoBehaviour {
     public int TotalCarriageAmount { get { return StoryCarriageAmount + FillerCarriageAmount; } }
     public int StoryCarriageAmount = 1;
     public int FillerCarriageAmount = 0;
+    int storyCount = 0;
+    int storyPlaced = 0;
+    int fillerCount = 0;
 
     public void OnEnable() {
         SceneManager.sceneLoaded += LevelLoad;
@@ -40,14 +43,14 @@ public class LevelGenerator : MonoBehaviour {
             StoryCarriageAmount = 1;
             Debug.LogError("Story Carriage Amount was Zero (0). Generation will continue with one forced Story Carriage.");
         }
-        int storyCount = 0;
-        int storyPlaced = 0;
-        int fillerCount = 0;
+        storyCount = 0;
+        storyPlaced = 0;
+        fillerCount = 0;
         GameObject toAddCandidate;
         //Select random carriages and place into list.
         while (_carriagesToPlace.Count < TotalCarriageAmount) {
             //The logic here is moved to a method to allow the filler carriage count to be 0. This avoids a DevideByZero.
-            if (StoryToFillerCalc(storyPlaced)) {
+            if (StoryToFillerCalc()) {
                 //Add Story Carriage
                 while (true) {
                     toAddCandidate = _storyCarriages[random.Next(_storyCarriages.Count - 1)];
@@ -89,9 +92,22 @@ public class LevelGenerator : MonoBehaviour {
         }
     }
 
-    bool StoryToFillerCalc(int storyPlaced) {
-        if (FillerCarriageAmount < 1) return false;
-        return storyPlaced < Mathf.Floor(StoryCarriageAmount / FillerCarriageAmount);
+    bool StoryToFillerCalc() {
+        //returns true to place story carriage. returns false to place a filler carriage.
+
+        float storyf = StoryCarriageAmount;
+        float fillerf = FillerCarriageAmount;
+        //If the filler amount is 0 OR if the filler count is now the same as the amount placed, return true.
+        if (FillerCarriageAmount < 1 || fillerCount >= FillerCarriageAmount) return true;
+        //If the ratio is even, subtract one from the calculation.
+        if ((storyf % fillerf) == 0) {
+            return storyPlaced < Mathf.Floor(storyf / fillerf) - 1;
+        }
+        //Ratio is odd
+        else {
+            return storyPlaced < Mathf.Floor(storyf / fillerf);
+        }
+        
     }
 
     void GenerateLevel() {
