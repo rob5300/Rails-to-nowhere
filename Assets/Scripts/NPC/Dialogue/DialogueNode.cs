5a469bf830a376c1dca254b5fce6ce5d52affd74
Matt Jones <matthewj866@gmail.com>
2017-03-10 01:10:14 +0000

    Updated DialogueNode, UI crashed and burned when I attempted changes - discarded them.

--- Assets/Scripts/NPC/Dialogue/DialogueNode.cs
+++ Assets/Scripts/NPC/Dialogue/DialogueNode.cs
@@ -36,6 +36,11 @@ public class DialogueNode {
         }
     }
 
+	public DialogueNode()
+	{
+
+	}
+
     public DialogueNode(string key, string text) {
         Key = key.ToLower();
         Text = text;
