9de97eca8615c5e2c971b166af45357a4e27fd05
Matt Jones <matthewj866@gmail.com>
2017-03-22 19:02:45 +0000

    Added functionality for serialising GameObjects.

--- Assets/Scripts/NPC/FillerNPC.cs
+++ Assets/Scripts/NPC/FillerNPC.cs
@@ -6,4 +6,18 @@ public class FillerNPC : NPC {
 
     public static List<FillerNPC> FillerNPCs = new List<FillerNPC>();
 
+	public FillerNPC()
+	{
+		TestProp = new List<GameObject>();
+	}
+
+	public List<GameObject> TestProp { get; set; }
+
+	public override List<string> GetSerialiseTargets()
+	{
+		List<string> props = base.GetSerialiseTargets();
+		props.Add("TestProp");
+		return props;
+	}
+
 }
